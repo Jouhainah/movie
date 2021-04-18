8bf9485e0fb8e6c7b6136de7c5ee066ac5163c99
Jouhaina <jouhainahmida@gmail.com>
2021-04-18 07:39:43 +0100

    add formGroup

--- src/app/pages/loginscreen/loginscreen.page.ts
+++ src/app/pages/loginscreen/loginscreen.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-loginscreen',
   templateUrl: './loginscreen.page.html',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginscreenPage implements OnInit {
 
-  constructor() { }
+  validationUserMessage ={
+    email:[
+      {type:"required", message:"Please enter your Email"},
+      {type:"pattern", message:"The Email entered is Incorrect.Try again"}
+    ],
+    password:[
+      {type:"required", message:"please Enter your Password!"},
+      {type:"minlength", message:"The Password must be at least 6 characters or more"}
+
+    ]
+  }
+  
+   validationFormUser: FormGroup;
+
+  constructor(public formbuilder: FormBuilder, private nav: NavController) { }
 
   ngOnInit() {
+
+
+    this.validationFormUser = this.formbuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ]))
+    })
+  }
+  
+  LoginUser(value){
+    console.log("Logged In")
   }
 
+  registerUser(){ 
+    this.nav.navigateForward(['signup'])
+  }
+  
 }
+
