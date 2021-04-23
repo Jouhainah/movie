4210f0956f7398c3c41d7a7d25c0e3ec7e23d564
Jouhaina <jouhainahmida@gmail.com>
2021-04-23 02:08:10 +0100

    add formGroup

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  validationMessage = {
+      name:[{type:"required", message:"Please enter your Full Name"}],
+      phone:[{type:"required", message:"Please enter your Phone No."},
+            {type:"pattern",message:"Please, Enter 8 digit Mobile Number.."}] ,
+      email:[{type:"required", message:"Please enter your Email Adress"},
+            {type:"pattern",message:"The Email entered is Incorrect.Try again.."}
+       ], 
+      password:[{type:"required", message:"password is required here "},
+                {type:"minlength",message:"The Password must be at least 6 characters or more"}
+        ], 
   }
 
+  ValidationFormUSer: FormGroup;
+  
+  constructor(public formbuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.ValidationFormUSer = this.formbuilder.group({
+      name: new FormControl('', Validators.compose([
+         Validators.required
+      ])),
+  
+      phone: new FormControl('', Validators.compose([
+        Validators.required,
+        , Validators.pattern("^[0-9]{8}$")
+      ])),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+  
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ]))
+  
+    })
+  
+  }
+
+  registerUser(value){}
 }
